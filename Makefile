788b82172155e9aeef640dad1f17976a45e9fd25
ImmortalPC <guillaumenuel@hotmail.com>
2014-01-05 20:58:53 +0100

    V1

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+NAME_internal = PCI_IO_GOD
+# Voir /proc/devices
+MAJOR_internal = 2544
+
 obj-m += PCI_IO_GOD.o
 
 default:
@@ -8,11 +12,21 @@ clean:
 
 in:
 	insmod ./PCI_IO_GOD.ko
+#	b      create a block (buffered) special file
+#	c, u   create a character (unbuffered) special file
+#	p      create a FIFO
+	mknod /dev/$(NAME_internal) c $(MAJOR_internal) 0
+
 
 out: rm
 rm:
 	rmmod PCI_IO_GOD.ko
+	rm /dev/PCI_IO_GOD
 
 sh: show
 show:
-	dmesg
+	dmesg | tail
+
+test:
+	python -c 'fp=open("/dev/PCI_IO_GOD", "w"); fp.write("123 456"); fp.close();'
+	dmesg | tail
--- PCI_IO_GOD.c
+++ PCI_IO_GOD.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>// Pour créer un accès à /dev/...
+#include <asm/io.h>// outb, ... (Voir cat /proc/ioports pour les adresses)
+// Voi aussi cat /proc/bus/pci/devices > 1.txt
 
 MODULE_AUTHOR("Immortal-PC");
 MODULE_DESCRIPTION("PCI-IO-By-God");
@@ -8,9 +10,9 @@ MODULE_SUPPORTED_DEVICE("none");
 MODULE_LICENSE("GPL");
 #define DEV_NAME "PCI_IO_GOD"
 
-static int major = 254;// numéro majeur du driver, 0 indique que l'on souhaite une affectation dynamique
+static int major = 2544;// numéro majeur du driver, 0 indique que l'on souhaite une affectation dynamique
 module_param(major, int, 0);
-MODULE_PARM_DESC(major, "major numbe");
+MODULE_PARM_DESC(major, "major number");
 
 #define stdError( lvl, msg, ... ) printk(lvl "[" __FILE__ ":%d]: " msg "\n", __LINE__, ##__VA_ARGS__)
 
@@ -20,27 +22,46 @@ MODULE_PARM_DESC(major, "major numbe");
 
 static ssize_t readDev( struct file* file, char* buf, size_t count, loff_t* ppos )
 {
-	printk(KERN_DEBUG "read()\n");
+	stdError(KERN_DEBUG, "read()");
 	return 0;
 }
 
 static ssize_t writeDev( struct file* file, const char* buf, size_t count, loff_t* ppos )
 {
-	printk(KERN_DEBUG "write()\n");
-	return 0;
+	int i=0;
+	int spacePos = -1;
+
+	if( !count ){
+		stdError(KERN_DEBUG, "write(): Empty write => Correct usage: echo '<id Card> <value>' > /dev/PCI_IO_GOD");
+		return -1;
+	}
+	for( i=0; buf[i]; ++i )
+	{
+		if( buf[i] == ' ' ){
+			spacePos = i;
+		}else if( !('0' <= buf[i] && buf[i] <= '9') ){
+			stdError(KERN_DEBUG, "write(): Bad write (%c) => Correct usage: echo '<id Card> <value>' > /dev/PCI_IO_GOD", buf[i]);
+			return -1;
+		}
+	}
+
+	stdError(KERN_DEBUG, "write() %s", buf);
+	return count;
 }
 
 static int openDev( struct inode* inode, struct file* file )
 {
-	printk(KERN_DEBUG "open()\n");
+	stdError(KERN_DEBUG, "open()");
 	return 0;
 }
 
 static int closeDev( struct inode* inode, struct file* file )
 {
-	printk(KERN_DEBUG "close()\n");
+	stdError(KERN_DEBUG, "close()");
 	return 0;
 }
+
+
 
 
 
@@ -55,10 +76,6 @@ struct file_operations fops =
 	.open = openDev,
 	.release = closeDev// correspond a close
 };
-
-
-
-
 
 
 
@@ -81,6 +98,7 @@ struct file_operations fops =
 static int __init entryPoint(void)
 {
 	int ret=0;
+	stdError(KERN_DEBUG, "---------------------------------------------------");
 	stdError(KERN_DEBUG, "Init " DEV_NAME);
 
 	// major:		Numéro majeur du driver, 0 indique que l'on souhaite une affectation dynamique.
@@ -88,10 +106,17 @@ static int __init entryPoint(void)
 	// fops:		Pointeur vers une structure qui contient des pointeurs de fonction.
 	// 				Ils définissent les fonctions appelées lors des appels systèmes (open, read...) du côté utilisateur.
 	if( (ret=register_chrdev(major, DEV_NAME, &fops)) < 0 ){
-		stdError(KERN_WARNING, " Erreur avec register_chrdev=%d ", ret);
+		if( ret == -EINVAL ){
+			stdError(KERN_WARNING, "Erreur avec register_chrdev=%d => Le nombre specifie n'est pas valide (> MAX_CHRDEV)", ret);
+		}else if( ret == -EBUSY ){
+			stdError(KERN_WARNING, "Erreur avec register_chrdev=%d => Le nombre majeur est occupe.", ret);
+		}else{
+			stdError(KERN_WARNING, "Erreur avec register_chrdev=%d", ret);
+		}
+		return ret;
 	}
 
-	stdError(KERN_DEBUG, "Ready " DEV_NAME);
+	stdError(KERN_DEBUG, "Ready " DEV_NAME " Major=%d", major);
 	return 0;
 }
 
